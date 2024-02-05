1c39791a43d4883fb4ec9943e1dfeb317c8bf20d
ste-cartu <ste.cartu@gmail.com>
2024-02-05 18:31:01 +0100

    commit

--- functions.py
+++ functions.py
@@ -1,93 +1,114 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from classy import Class
+import os
 
 
 
-# funzione che, data in input la massa del neutrino, restituisce il power density spectrum P(k,a) e il growth factor D(a) e D(k,a)
-
-def DataFromClass(m_neutrino = 0.06) :
-    # creo il modello CLASS, imposto i suoi parametri e lo eseguo
-    LCDM = Class()
-
-    LCDM.set({'omega_b': 0.02238280, 
-            'omega_cdm': 0.1201075, 
-            'h': 0.67810, 
-            'A_s': 2.100549e-09, 
-            'n_s':0.9660499, 
-            'tau_reio':0.05430842, 
-            'z_max_pk':10,
-            'N_ncdm': 1,
-            'm_ncdm': m_neutrino          # massa del neutrino in eV
-            })
-    LCDM.set({'output': 'tCl,pCl,lCl,mPk',
-            'lensing':'yes',
-            'P_k_max_h/Mpc': 10, 
-            'z_max_pk': 1})
-
-    LCDM.compute()
+# funzione che, data in input la massa del neutrino, restituisce il power density spectrum P(k,z) o P(k,a) e il growth factor D(z) o D(a) e D(k,z) o D(k,a)
 
 
+def DataFromCLASS(dim_k = 500, dim_z=200, m_neutrino = 0.06, path = 'files') :
+        # creo il modello CLASS, imposto i suoi parametri e lo eseguo
+        LCDM = Class()
 
-    # dal modello estraggo il background
-    bg = LCDM.get_background()
-    # h = LCDM.h()
+        LCDM.set({'omega_b': 0.02238280, 
+                'omega_cdm': 0.1201075, 
+                'h': 0.67810, 
+                'A_s': 2.100549e-09, 
+                'n_s':0.9660499, 
+                'tau_reio':0.05430842, 
+                'z_max_pk':10,
+                'N_ncdm': 1,
+                'm_ncdm': m_neutrino          # massa del neutrino in eV
+                })
+        LCDM.set({'output': 'tCl,pCl,lCl,mPk',
+                'lensing':'yes',
+                'P_k_max_h/Mpc': 10, 
+                'z_max_pk': 10})
 
-    # redshift z = [0, 10] e conversione in fattore di scala a
-    zz = bg['z'][37000:]     # dell'array di redshift seleziono solo la parte compresa tra z=0 e z=10.2
-    aa = 1./(zz+1)
-    n = len(zz)
+        LCDM.compute()
+        h = LCDM.h()
 
-    # print(aa[0])
-    # print(aa[-1])
-    # print(n)
+        # redshift z = [0, 10] e scala k = [10^-4; 3]
+        nk = dim_k
+        nz = dim_z
+        zz = np.linspace(0, 10, nz)
+        kk = np.logspace(-4, np.log10(3), nk)*h    # k in 1/Mpc
 
-
-    # growth factor scale independent D(a)
-    Da = bg['gr.fac. D'][37000:]
-    np.save('D(a)_m-neu=' + str(m_neutrino), Da)
-
-
-    # definisco il range di scale k = [10^-4; 3] con n punti e poi calcolo il growth factor scale dependent D(k,a) normalizzato su D(k,1)
-    kk = np.logspace(-4, np.log10(3), n)    # k in h/Mpc
-
-
-    # pka = [[0 for i in range(n)] for j in range(n)]
-    Pka = np.zeros([n,n])
-
-    # calcolo il Power Spectrum Pka(k,a) per tutti i valori di k e a
-    for k in range(n) :
-        for a in range(n) :
-    #       pka[k][a] = LCDM.pk(kk[k], aa[a])
-            Pka[k,a] = LCDM.pk(kk[k], aa[a])
-
-    # Pka = np.array(pka)
-    np.save('P(k,a)_m-neu=' + str(m_neutrino), Pka)
-
-    # se z=0 => a=1
-    # estraggo il growth factor normalizato dal rapporto Pka(k,a)/Pka(k,1)
-    # d = [[0 for i in range(n)] for j in range(n)]
-    Dka = np.zeros([n,n])
-
-    for k in range(n) :
-        for a in range(n) :
-    #       d[k][a] = np.sqrt(pka[k][a]/pka[k][-1])
-            Dka[k,a] = np.sqrt(Pka[k,a]/Pka[k,-1])
-    
-
-    #Dka = np.array(d)
-    np.save('D(k,a)_m-neu=' + str(m_neutrino), Dka)
+        filename = 'redshift'
+        save = os.path.join(path, filename)
+        np.save(save, zz)
+        filename = 'scale'
+        save = os.path.join(path, filename)
+        np.save(save, kk)
 
 
-    # plotto D(k,a) con variabile a e parametro k, plotto anche D(a)
+        # growth factor scale independent D(z)
+        Dz = np.array([LCDM.scale_independent_growth_factor(z) for z in zz])
+        filename = 'D(z)_m-neu=' + str(round(m_neutrino, 3))
+        save = os.path.join(path, filename)
+        np.save(save, Dz)
 
-    # plt.plot(aa, Dka[0,:], color='blue', label='D(' + str(kk[0]) + ',a)') # , linewidth='0.5')
-    # plt.plot(aa, Dka[2976,:], color='blue', label='D(' + str(kk[2976]) + ',a)') # , linewidth='0.5')
-    # plt.plot(aa, Da, color='red', label='D(a)')
-    # plt.axhline(y=1, color='green', linestyle='--')
 
-    # plt.xlabel('a')
-    # plt.ylabel('D')
+        # calcolo il Power Spectrum Pkz(k,z) per tutti i valori di k e z
+        Pkz = np.zeros([nk,nz])
+        for k in range(nk) :
+                for z in range(nz) :
+                        Pkz[k,z] = LCDM.pk_lin(kk[k], zz[z])*(h**3)
+        filename = 'P(k,z)_m-neu=' + str(round(m_neutrino, 3))
+        save = os.path.join(path, filename)
+        np.save(save, Pkz)
 
-    # plt.legend()
-    # plt.show()
+
+        # estraggo il growth factor normalizato dal rapporto Pka(k,z)/Pka(k,0)
+        Dkz = np.zeros([nk,nz])
+        for k in range(nk) :
+                for z in range(nz) :
+                        Dkz[k,z] = np.sqrt(Pkz[k,z]/Pkz[k,0])
+        filename = 'D(k,z)_m-neu=' + str(round(m_neutrino, 3))
+        save = os.path.join(path, filename)
+        np.save(save, Dkz)
+
+
+        # estraggo i mu
+        mu = np.zeros([nk,nz])
+        for k in range(nk) :
+                mu[k,:] = Dkz[k,:] / (Dz/Dz[0])
+        filename = 'Mu(k,z)_m-neu=' + str(round(m_neutrino, 3))
+        save = os.path.join(path, filename)
+        np.save(save, mu)
+
+        return {'redshift': zz, 'scale':kk, 'power_spectrum': Pkz, 'growth_numeric': Dz, 'growth_analitic': Dkz, 'growth_ratio': mu}
+
+        # plotto D(k,z) con variabile z e parametro k, plotto anche D(z)
+'''
+        plt.plot(zz, Dkz[0,:], color='blue', label='D(' + str(kk[0]) + ',z)', linewidth='0.5')
+        plt.plot(zz, Dkz[n-1,:], color='blue', label='D(' + str(kk[n-1]) + ',z)', linewidth='0.5')
+        plt.plot(zz, Dz, color='red', label='D(z)', linewidth='0.5')
+        plt.axhline(y=1, color='green', linestyle='--')
+
+        plt.xlabel('z')
+        plt.ylabel('D(z)')
+
+        plt.legend()
+        plt.show()
+'''
+
+
+
+
+
+
+def Masses(min = 0.06, max = 1, dim = 10, path = 'files') :
+
+        filename = 'neutrino_mass.npy'
+        save = os.path.join(path, filename)
+
+        m = np.linspace(min, max, dim)
+        np.save(save, m)
+
+        return m
+
+
+# DataFromCLASS(m_neutrino = 0.06)
