bc06f1d3c69efb8b2153ae9be1979d2ac1680ba1
ste-cartu <ste.cartu@gmail.com>
2024-02-24 09:38:59 +0100

    symbolic regression for Mu(m,k,z)

--- sim_mu.py
+++ sim_mu.py
@@ -16,56 +16,15 @@ nk = 100        # dimensione array dei k
 nz = 20         # dimensione array dei redshift
 
 # parametri del modello
-ni = 50       # numero di iterazioni del modello                                  DEFAULT 40 / BEST 5000
-comp = 20       # complessità del modello (numero degli elementi nell'equazione)    DEFAULT 20 / BEST 50
-pop = 15       # numero di popolazioni da cui parte il modello                     DEFAULT 15 / BEST 150
+ni = 4000       # numero di iterazioni del modello                                  DEFAULT 40 / BEST 4000
+comp = 40       # complessità del modello (numero degli elementi nell'equazione)    DEFAULT 20 / BEST 40
+pop = 120       # numero di popolazioni da cui parte il modello                     DEFAULT 15 / BEST 120
 
-'''
-nm = int(sys.argv[1])
-nk = int(sys.argv[2])
-nz = int(sys.argv[3])
-ni = int(sys.argv[4])
-comp = int(sys.argv[6])
-pop = int(sys.argv[5])
-'''
+# per inserire i parametri da terminale
+# ni = int(sys.argv[1])
+# comp = int(sys.argv[2])
+# pop = int(sys.argv[3])
 
-# ––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
-# DEFINIZIONE VARIABILI
-
-if os.path.exists('files/neutrino_mass_[' + str(nm) +'].npy') :
-    mm = np.load('files/neutrino_mass_[' + str(nm) +'].npy')
-else :
-    mm = f.Masses(0.06, 1, nm, 'files')
-
-if os.path.exists('files/Mu(k,z)_[' + str(nk) + ',' + str(nz) + ']_m=0.06.npy') :
-    mu = np.zeros([nm, nk, nz])
-    for m in range(nm) :
-        mupath = 'files/Mu(k,z)_[' + str(nk) + ',' + str(nz) + ']_m=' + str(round(mm[m], 3)) + '.npy'
-        mu[m,:,:] = np.load(mupath)
-else :
-    cosmos = []
-    mus =  []
-    for i in range(nm) :
-        cosmos.append(f.DataFromCLASS(nk, nz, mm[i], 'files'))
-        mus.append(cosmos[i]['growth_ratio'])
-    mu = np.array(mus)
-
-    # controllo su mu
-    if (len(mu[:,0,0]) != nm) :
-        print('Error! dim(Mu[:,0,0]) ≠ dim(mass)')
-    elif (len(mu[0,:,0]) != nk) :
-        print('Error! dim(Mu[0,:,0]) ≠ dim(scale)')
-    elif (len(mu[0,0,:]) != nz) :
-        print('Error! dim(Mu[0,:,0]) ≠ dim(redshift)')
-    
-    for m in range(nm) :
-        for k in range(nk) :
-            for z in range(nz) :
-                if mu[m,k,z] - mus[m][k][z] != 0 :
-                    print('Error! mu_list['+str(m)+','+str(k)+','+str(z)+'] ≠ mu_array['+str(m)+','+str(k)+','+str(z)+']')
-
-kk = np.load('files/scale_[' + str(nk) +'].npy')
-zz = np.load('files/redshift_[' + str(nz) +'].npy')
 
 
 # ––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
@@ -74,21 +33,13 @@ zz = np.load('files/redshift_[' + str(nz) +'].npy')
 if os.path.exists('files/data_[' + str(nm) + ',' + str(nk) + ',' + str(nz) + '].npy') :
     data = np.load('files/data_[' + str(nm) + ',' + str(nk) + ',' + str(nz) + '].npy')
 else :
-    data = np.zeros([nm*nk*nz, 4])
-    for m in range(nm) :
-        for k in range(nk) :
-            for z in range(nz) :
-                data[m*nk*nz+k*nz+z,0] = mm[m]
-                data[m*nk*nz+k*nz+z,1] = kk[k]
-                data[m*nk*nz+k*nz+z,2] = zz[z]
-                data[m*nk*nz+k*nz+z,3] = mu[m,k,z]
+    os.system(f'python3 gen_data.py {nm} {nk} {nz}')
+    data = np.load('files/data_[' + str(nm) + ',' + str(nk) + ',' + str(nz) + '].npy')
 
-            #print(data[m*nk*nz+k*nz+z,0],'\t',data[m*nk*nz+k*nz+z,1],'\t',data[m*nk*nz+k*nz+z,2],'\t',data[m*nk*nz+k*nz+z,3])
-    
-    np.save('files/data_[' + str(nm) + ',' + str(nk) + ',' + str(nz) + ']', data)
 
 # l'array 'data' è così strutturato:
 # masse neutrino [eV] | k [1/Mpc] (scala) | redshift | valore di mu
+
 
 
 # ––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
@@ -113,6 +64,6 @@ else :
         loss = 'L2DistLoss()',
         model_selection = 'best',
         equation_file = 'models/mu(' + str(ni) + ',' + str(comp) + ',' + str(pop) + ').csv'
-        )
+    )
 
     model_mu.fit(data[:,:3], data[:,3])
